6896d4717587166a627132a9c6ad38c06bd8295e
Tiago Brandão <tiagomourabrandao@gmail.com>
2025-09-10 11:32:31 -0300

    commit #4

--- file2.py
+++ file2.py
@@ -1,4 +1,5 @@
 #file 2
 
-def lala(text: float):
-    return text * 2
+def lala(text: str, thing_that_will_trigger_conflict: int):
+    print("Now we should see a conflict when we merge the branch other_api_1 into the maps branch")
+    return thing_that_will_trigger_conflict * 2
